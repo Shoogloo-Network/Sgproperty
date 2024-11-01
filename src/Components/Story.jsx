c27f820e78b6cee6c5614bbeeaf67c443bca833b
Abhishek Singh <abhishek.singh@shoogloo.com>
2024-11-01 15:20:00 +0530

    Commit

--- src/Components/Story.jsx
+++ src/Components/Story.jsx
@@ -1,10 +1,34 @@
 
 import Stories from 'react-insta-stories';
+import Card from './Card';
 
 const Story = () => {
+  const CardData =[{
+    image:"/assets/images/desktop-banner.jpeg" ,
+          title:"Sample Card Title 1" ,
+          description:"This is a description for the sample card. It can hold any text you want to display.",
+  },{
+    image:"/assets/images/desktop-banner.jpeg" ,
+          title:"Sample Card Title 1" ,
+          description:"This is a description for the sample card. It can hold any text you want to display.",
+  },{
+    image:"/assets/images/desktop-banner.jpeg" ,
+          title:"Sample Card Title 1" ,
+          description:"This is a description for the sample card. It can hold any text you want to display.",
+  },{
+    image:"/assets/images/desktop-banner.jpeg" ,
+          title:"Sample Card Title 1" ,
+          description:"This is a description for the sample card. It can hold any text you want to display.",
+  },]
     const stories =[{
         url:"src/assets/stories/shriram-serenity-project-project-large-image1-7568.avif",
         duration:2000,
+        content:(props)=>(
+          <div style={{backgroundImage:'url("src/assets/stories/shriram-serenity-project-project-large-image1-7568.avif")', height:'100%',width:'100%',backgroundRepeat:'no-repeat'}}><Card  image="/assets/images/desktop-banner.jpeg" 
+          title="Sample Card Title 1" 
+          description="This is a description for the sample card. It can hold any text you want to display."/></div>
+          
+        ),
         
     },
     {
