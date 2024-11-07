2a2307bd7806d627b2282f652604e52e9d916a8c
Abhishek Singh <abhishek.singh@shoogloo.com>
2024-11-07 11:25:15 +0530

    Commit

--- src/Components/User.jsx
+++ src/Components/User.jsx
@@ -1,19 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react'
 import UserSectionList from './UserSectionList'
 import Activity from './Activity'
+import Profile from './Profile'
 const User = () => {
+    const [activeSection, setActiveSection] = useState('profile');
+    const handleClick=(item)=>{
+        console.log(item);
+        setActiveSection(item.id);
+    }
   return (
-    
+   
     <>
     <div style={{display:'flex',flexDirection:'row',gap:'20px'}}>
     <div>
-   <UserSectionList/>
+   <UserSectionList onClick={handleClick}/>
    </div>
    <div>
-    <Activity/>
+    {
+        activeSection ==='profile' && <Profile/>
+    }
    </div>
+   <div>
+    {
+        activeSection ==='activity' && <Activity/>
+    }
+
+   </div>
+   <div>
+    {
+        activeSection ==='interactions' && <Interactions/>
+    }
   </div>
-  
+  </div>
     </>
   )
 }
--- src/Components/UserSectionList.jsx
+++ src/Components/UserSectionList.jsx
@@ -1,75 +1,76 @@
 import React from 'react'
 import IconCard from './IconCard'
-const UserSectionList = () => {
-    const handleClick=(item)=>{
-        console.log(item);
-    }
+const UserSectionList = ({onClick}) => {
+ 
     const iconData =[
         {
             img:'src/assets/icon/verified.png',
            title:'PROFILE',
+           id:'profile'
 
         },
         {
             img:'src/assets/icon/home-button.png',
             title:'MY ACTIVITY',
+            id:'activity'
 
         },
         {
             img:'src/assets/icon/home-button.png',
             title:'MY INTERACTIONS',
-
+            id:'interactions'
         },
         {
             img:'src/assets/icon/home-button.png',
             title:'POST PROPERTY',
-
+            id:'postproperty'
         },
         {
             img:'src/assets/icon/home-button.png',
-            title:'MY LISTINGS',
-
+            title:'MY LISTINGS',    
+            id:'mylistings'
         },
         {
             img:'src/assets/icon/home-button.png',
             title:'MY COUPONS',
+            id:'mycoupons'
 
         },
         {
             img:'src/assets/icon/home-button.png',
             title:'MY AGENTS',
-
+            id:'myagents'
         },
         {
             img:'src/assets/icon/home-button.png',
             title:'MY SERVICES',
-
+            id:'myservices'
         },
         {
             img:'src/assets/icon/home-button.png',
             title:'RENTAL AGREEMENTS',
-
+            id:'rentalagreements'
         },
         {
             img:'src/assets/icon/home-button.png',
             title:'RENT PAYMENTS',
-
+            id:'rentpayments'
         },
         {
             img:'src/assets/icon/home-button.png',
             title:'REFER AND EARN',
-
+            id:'referandearn'
         },
         {
             img:'src/assets/icon/home-button.png',
             title:'LOGOUT',
-
+            id:'logout'
         },
     ] ;
 
   return (
    <>
-   <IconCard data={iconData} onClick={handleClick} style={{display:'flex',flexDirection:'column',gap:'10px'}} />
+   <IconCard data={iconData} onClick={onClick} style={{display:'flex',flexDirection:'column',gap:'10px'}} />
    </>
   )
 }
