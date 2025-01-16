0e9f817419f387e157a11b7567145b6d76e42277
Abhishek Singh <abhishek.singh@shoogloo.com>
2025-01-16 10:28:05 +0530

    Commit

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,68 +1,60 @@
-
-
-import Story from './Story'
-import IconCardBtnWithoutHover from './IconCardBtnWithoutHover'
-import RealStateBanner from './RealStateBanner'
-import './Home.css'
-import CardWithList from './CardWithList'
-import { useNavigate } from 'react-router-dom'
-
-
+import Story from "./Story";
+import IconCardBtnWithoutHover from "./IconCardBtnWithoutHover";
+import RealStateBanner from "./RealStateBanner";
+import "./Home.css";
+import CardWithList from "./CardWithList";
+import { useNavigate } from "react-router-dom";
 
 const Home = () => {
-  const iconDataForButton =[
+  const iconDataForButton = [
     {
-        img:'src/assets/icon/building-img.svg',
-        title:'Best Sellers in India',
-        description:'Our freshly brewed list of the best residential projects from top rated builders in the country, backed by our award-winning start-to-finish services.',
-      
-        buttonName:'View All',
+      img: "src/assets/icon/building-img.svg",
+      title: "Best Sellers in India",
+      description:
+        "Our freshly brewed list of the best residential projects from top rated builders in the country, backed by our award-winning start-to-finish services.",
 
+      buttonName: "View All",
     },
-]
+  ];
 
-const navigate = useNavigate();
-// const userName = JSON.parse(localStorage.getItem('user'));
-// const onClick = () => {
-//   if(!userName){
-//     navigate('/login');
-//   }
-// }
+  const navigate = useNavigate();
 
   return (
-    // onClick={onClick}
-    <div >
-   
-    <RealStateBanner/>
-   
-    <div className='home-container'>
-
-    <h1>Hot Selling Projects in India</h1>
-    
-   
-        <div className='home-content' style={{cursor:'pointer'}}>
-       
-      <IconCardBtnWithoutHover data={iconDataForButton} onClick={()=>{
-        navigate('/search');
-      }}/>
-      <Story/>
-
+    <div>
+      <RealStateBanner />
+      <div className="home-container">
+        <h1>Hot Selling Projects in India</h1>
+        <div className="home-content" style={{ cursor: "pointer" }}>
+          <IconCardBtnWithoutHover
+            data={iconDataForButton}
+            onClick={() => {
+              navigate("/search");
+            }}
+          />
+          <Story />
         </div>
         <div className="real-estate-sections">
-      <div className="real-estate-section">
-        <h1 className="section-title">For Real Estate Developers</h1>
-        <CardWithList url="src/assets/thumbnail/propvr-side.svg" backgroundColor="#ddf5ff" />
+          <div className="real-estate-section">
+            <h1 className="section-title">For Real Estate Developers</h1>
+            <CardWithList
+              url="src/assets/thumbnail/propvr-side.svg"
+              backgroundColor="#ddf5ff"
+            />
+          </div>
+          <div
+            className="real-estate-section"
+            style={{ backgroundColor: "#fff" }}
+          >
+            <h1 className="section-title">For Agents</h1>
+            <CardWithList
+              url="src/assets/thumbnail/square-connect-side.svg"
+              backgroundColor="#fff6e7"
+            />
+          </div>
+        </div>
       </div>
-      <div className="real-estate-section" style={{ backgroundColor: '#fff'}}>
-        <h1 className="section-title">For Agents</h1>
-        <CardWithList url="src/assets/thumbnail/square-connect-side.svg" backgroundColor="#fff6e7"/>
-      </div>
     </div>
-    </div>
-    
-    
-    </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
