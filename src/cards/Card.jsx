dccc4d81419c6689cf9aad2491b1ce93aed696dd
Abhishek Singh <abhishek.singh@shoogloo.com>
2025-01-28 11:15:27 +0530

    Commit

--- src/cards/Card.jsx
+++ src/cards/Card.jsx
@@ -1,59 +1,54 @@
-
 import './Card.css';
 import IconCard from './IconCard';
 import BtnPrimaryWithIcon from '../buttons/BtnPrimaryWithIcon';
 import { useNavigate } from 'react-router-dom';
-const Card = ({  title, description,descriptionPrice,iconCardData,onClick,url }) => {
+
+const Card = ({ title, description, descriptionPrice, iconCardData, onClick, url }) => {
   const navigate = useNavigate();
+
   const onImageClick = () => {
     navigate('/detail-page');
   };
-  
-  
+
   return (
-    <div className="card fade-in" style={{cursor:'pointer' , zIndex:1009}}>
-   
-
-{
-  url?( <div style={{display:'flex',flexDirection:'row'}}>
-  <div style={{width:'50%',padding:'10px',}}>
-    <img src={url} alt='card' style={{width:'100%',height:'100%',borderRadius:'10px'}} onClick={onImageClick}/>
-  </div>
-  <div className="card-content" onClick={onClick} style={{width:'50%'}}>
-    <h3 className="card-title">{title}</h3>
-    <p className="card-description">{description}</p>
-    <p className="card-description-color" style={{color:'#5f449b',fontSize:'18px',fontWeight:'bold' , marginTop:'5px'}}>{descriptionPrice}</p>
-    <div >
-      <div >
-        <IconCard data={iconCardData}/>
-      </div>
-      
-    </div>
-    <BtnPrimaryWithIcon iconName='FaPhone' cnt="Get a Callback"  />
-  </div>
- </div>
-  ):(
-  <div className="card-content" onClick={onClick}>
-  <h3 className="card-title">{title}</h3>
-  <p className="card-description">{description}</p>
-  <p className="card-description-color">{descriptionPrice}</p>
-  <div >
-    <div >
-      <IconCard data={iconCardData}/>
-    </div>
-    
-  </div>
-  <BtnPrimaryWithIcon iconName='FaPhone' cnt="Get a Callback"  />
-</div>
-)}
-
-
-
-
-
-     
+    <div className="card fade-in card-cursor">
+      {url ? (
+        <div className="card-flex-row">
+          <div className="card-image-wrapper">
+            <img
+              src={url}
+              alt="card"
+              className="card-image-style"
+              onClick={onImageClick}
+            />
+          </div>
+          <div className="card-content card-content-half" onClick={onClick}>
+            <h3 className="card-title">{title}</h3>
+            <p className="card-description">{description}</p>
+            <p className="card-description-color">{descriptionPrice}</p>
+            <div>
+              <div>
+                <IconCard data={iconCardData} />
+              </div>
+            </div>
+            <BtnPrimaryWithIcon iconName="FaPhone" cnt="Get a Callback" />
+          </div>
+        </div>
+      ) : (
+        <div className="card-content" onClick={onClick}>
+          <h3 className="card-title">{title}</h3>
+          <p className="card-description">{description}</p>
+          <p className="card-description-color">{descriptionPrice}</p>
+          <div>
+            <div>
+              <IconCard data={iconCardData} />
+            </div>
+          </div>
+          <BtnPrimaryWithIcon iconName="FaPhone" cnt="Get a Callback" />
+        </div>
+      )}
     </div>
   );
-}
+};
 
 export default Card;
